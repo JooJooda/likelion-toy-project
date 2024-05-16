43f40903190838733912ac4b85c4f4e6daf9aa10
JooJooda <liyingzhou375@gmail.com>
2024-05-16 15:43:48 +0900

    fix : Post user 필드를 writer로 수정

--- toy-project/posts/models.py
+++ toy-project/posts/models.py
@@ -12,6 +12,6 @@ class Post(BaseModel):
     id = models.AutoField(primary_key=True)
     title = models.CharField(verbose_name="제목", max_length=20)
     content = models.TextField(verbose_name="내용")
-    user = models.CharField(verbose_name="작성자", max_length=10)
+    writer = models.CharField(verbose_name="작성자", max_length=10)
     password = models.CharField(verbose_name="비밀번호", max_length=10)
 
