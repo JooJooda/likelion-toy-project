b3a192ce8e87dc454ee309129dc0b6083812983c
JooJooda <liyingzhou375@gmail.com>
2024-05-23 16:25:03 +0900

    fix : cors 오류 수정

--- toy-project/config/settings.py
+++ toy-project/config/settings.py
@@ -86,6 +86,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [ 
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:5500",
 ]
 
 ROOT_URLCONF = 'config.urls'
